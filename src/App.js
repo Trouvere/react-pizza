6aa06dc80399a38d598aa1cf4fece7f7b19fb428
Mishyn Aliaksei <trouvere777@mail.ru>
2020-07-15 23:11:30 +0300

    add folder Components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import Header from "./Header";
-import Button from "./Button";
+import Header from "./components/Header";
+import Button from "./components/Button";
 
 function App() {
   return (
