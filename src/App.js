7d2d32169ab25f2b95a5271f19b39cac5fbd15cf
Mishyn Aliaksei <trouvere777@mail.ru>
2020-07-16 23:14:16 +0300

    Add Categories (with useState), index

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 
-import Header from "./components/Header";
-import Button from "./components/Button";
+import { Categories, Header, Button } from "./components";
 
 function App() {
   return (
@@ -12,16 +11,17 @@ function App() {
       <div className="content">
         <div className="container">
           <div className="content__top">
-            <div className="categories">
-              <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
-              </ul>
-            </div>
+            <Categories
+              onClickItem={(name) => console.log(name)}
+              items={[
+                "Мясные",
+                "Вегетарианская",
+                "Гриль",
+                "Острые",
+                "Закрытые",
+              ]}
+            />
+
             <div className="sort">
               <div className="sort__label">
                 <svg
